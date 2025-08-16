47b0639e2e0978467a92101b2e676efff057ed1d
sahipe <sahipe11@gmail.com>
2025-08-16 13:12:09 +0530

    fix: ensure AgencyChannel Excel export includes correct dateTime formatting

--- server/server.js
+++ server/server.js
@@ -128,10 +128,8 @@ app.get("/api/agency/excel", async (req, res) => {
     const formattedData = agencyData.map((a) => ({
       "Employee Name": a.employeeName,
       Designation: a.designation,
-      "Date & Time": a.visitingDateTime
-        ? dayjs(a.visitingDateTime)
-            .tz("Asia/Kolkata")
-            .format("DD-MM-YYYY hh:mm A")
+      "Date & Time": a.dateTime
+        ? dayjs(a.dateTime).tz("Asia/Kolkata").format("DD-MM-YYYY hh:mm A")
         : "",
       "Number of Partner Meet": a.numberOfPartnerMeet,
       "Motor Login Premium": a.motorLoginPremium,
